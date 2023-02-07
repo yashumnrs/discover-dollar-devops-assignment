f49c39c75c2ed4491ade23d9b76b63d3430d44fd
yashumnrs <91306937+yashumnrs@users.noreply.github.com>
2023-02-07 22:26:18 +0530

    Delete yashu-terraform directory

--- yashu-terraform/modules/network_interface/outputs.tf
+++
@@ -1,3 +0,0 @@
-output "id" {
-  value = azurerm_network_interface.ni.*.id
-}
--- yashu-terraform/modules/resource_group/main.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_resource_group" "rg" {
-  count    = length(var.eus_rg_name_list)
-  name     = element(var.eus_rg_name_list, count.index)
-  location = element(var.eus_rg_location, count.index)
-  tags     = var.tags
-}
